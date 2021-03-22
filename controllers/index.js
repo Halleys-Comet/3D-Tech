d980bbb27b6ae7cf2ac52f75144d356d4cdc0b8b
Halleys-Comet <jhalley230@gmail.com>
2021-03-21 22:01:31 -0500

    final

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
